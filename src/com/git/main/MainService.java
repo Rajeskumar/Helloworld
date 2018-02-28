74ed69ba081f6312587054324b2fee3daa4f354f
Rajeskumar <rajeskumarcse@gmail.com>
2018-02-27 19:14:42 -0600

    First Commit

---
+++ src/com/git/main/MainService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.git.main;
+
+/**
+ * @author rajesh
+ *
+ */
+public class MainService {
+	
+	public static void main (String args[]) {
+		System.out.print("First Git Setup Project");
+	}
+
+}
